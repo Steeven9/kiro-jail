de9c144f7aabeb2c44174bfa52a348b09de90e67
Stefano Taillefert <stefano.taille@gmail.com>
2026-04-03 15:54:06 +0200

    Make script more modular

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM alpine:3.23
 LABEL author="JamesClonk <jamesclonk@jamesclonk.ch>"
 
-RUN apk --no-cache add ca-certificates go curl bash unzip jq git gcompat
+RUN apk upgrade --no-cache && \
+    apk add --no-cache ca-certificates go curl bash unzip jq git gcompat
 
 RUN addgroup -g 501 kiro && \
     adduser -u 501 -G kiro -h /home/kiro -s /bin/bash -D kiro
+
 RUN USER=kiro && \
     GROUP=kiro && \
     curl -SsL https://github.com/boxboat/fixuid/releases/download/v0.6.0/fixuid-0.6.0-linux-amd64.tar.gz | tar -C /usr/local/bin -xzf - && \
@@ -12,6 +14,7 @@ RUN USER=kiro && \
     chmod 4755 /usr/local/bin/fixuid && \
     mkdir -p /etc/fixuid && \
     printf "user: $USER\ngroup: $GROUP\n" > /etc/fixuid/config.yml
+
 USER kiro:kiro
 
 ARG KIRO_VERSION=1.28.3
@@ -27,4 +30,3 @@ VOLUME /home/kiro/project
 WORKDIR /home/kiro/project
 
 ENTRYPOINT ["fixuid"]
-
--- kiro-jail.sh
+++ kiro-jail.sh
@@ -1,16 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-source ~/.aws_identity_provider || true
+DOCKER_IMAGE_NAME="localhost/kiro-jail"
+AWS_CONFIG_LOCATION="${HOME}/.aws_identity_provider"
+KIRO_CONFIG_LOCATION="${HOME}/.kiro"
+
+echo "== Reading config... =="
+if [[ -f "${AWS_CONFIG_LOCATION}" ]]; then
+	source "${AWS_CONFIG_LOCATION}"
+	echo "Config loaded from ${AWS_CONFIG_LOCATION}"
+else
+	echo "Using defaults - see README to customize"
+fi
+
 export AWS_IDENTITY_PROVIDER_URL=${AWS_IDENTITY_PROVIDER_URL:-https://your-value.awsapps.com/start}
 export AWS_REGION=${AWS_REGION:-eu-central-1}
 export KIRO_JAIL=$(basename $(pwd))
 export KIRO_UID=$(id -u)
 export KIRO_GID=$(id -g)
 
+# build the image if not found locally
+if ! podman images | grep "${DOCKER_IMAGE_NAME}" >/dev/null; then
+	echo "== Image ${DOCKER_IMAGE_NAME} not found locally, building it... =="
+	podman build . -t "${DOCKER_IMAGE_NAME}"
+fi
+
+mkdir -p "${KIRO_CONFIG_LOCATION}"
+
+echo "== Starting container... =="
 podman run -it --rm \
-	-v ~/.kiro:/home/kiro/.kiro:rw,Z \
-	-v "$(pwd)":/home/kiro/project:rw,Z \
+	-v "${KIRO_CONFIG_LOCATION}:/home/kiro/.kiro:rw,Z" \
+	-v "$(pwd):/home/kiro/project:rw,Z" \
 	-w /home/kiro/project \
-	-u ${KIRO_UID}:${KIRO_GID} --userns keep-id \
+	-u "${KIRO_UID}:${KIRO_GID}" --userns keep-id \
 	--name "${KIRO_JAIL}" \
-	kiro-jail:latest /bin/bash -c "kiro-cli login --identity-provider ${AWS_IDENTITY_PROVIDER_URL} --region ${AWS_REGION} --use-device-flow; kiro-cli"
+	"${DOCKER_IMAGE_NAME}" /bin/bash \
+	-c "kiro-cli login --identity-provider ${AWS_IDENTITY_PROVIDER_URL} --region ${AWS_REGION} --use-device-flow; kiro-cli"
